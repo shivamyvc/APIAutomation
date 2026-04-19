cdda7b718c5ddc99ea8eb89f3a5b563cf04513ec
Shivam <shivamyvc@gmail.com>
2026-04-19 17:39:52 +0530

    Report Path Modified

--- src/main/java/utilities/Reporter/ExtentReportManager.java
+++ src/main/java/utilities/Reporter/ExtentReportManager.java
@@ -35,7 +35,7 @@ public class ExtentReportManager {
 
 		String folder = new SimpleDateFormat("dd-MMM-yyyy").format(new Date());
 		String subfolder =       ""     ;//new SimpleDateFormat("hh-mm-ss").format(new Date());
-		return EXTENT_REPORT_FOLDER + "/" + folder + "/" + subfolder + "_Report.html";
+		return EXTENT_REPORT_FOLDER + "/Report.html";
 
 	}
 	
